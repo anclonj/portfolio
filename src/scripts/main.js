5fe7a5afd0fc9f7ef325960963a7c7a4df555a02
Aleksandr Ivanov <alexandr.ivanow@hotmail.com>
2018-03-19 20:27:09 +0300

    about.html

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+window.initMap = () => {
+    var uluru = {
+        lat: 55.49589, 
+        lng: 37.28094
+    };
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: uluru
+    });
+
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+}
