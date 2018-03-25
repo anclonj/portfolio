4bfa35609960cbd36d87593d02935eafc3dc02f7
Aleksandr Ivanov <alexandr.ivanow@hotmail.com>
2018-03-26 00:59:07 +0300

    works.pub

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,16 @@
-window.onresize = function() {
-  let height = document.getElementById('top').offsetHeight;
-  console.log(height);
-  document.getElementById('topbg').style.height = height;
-}
-
 window.initMap = () => {
-    var uluru = {
-        lat: 55.49589, 
-        lng: 37.28094
-    };
+  var uluru = {
+    lat: 55.49589, 
+    lng: 37.28094
+  };
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 15,
-      center: uluru
-    });
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: uluru
+  });
 
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
 }
